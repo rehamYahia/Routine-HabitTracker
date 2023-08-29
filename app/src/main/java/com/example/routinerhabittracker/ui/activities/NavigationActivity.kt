2d5c07d5586e55ddbb67f699def9e805bcf7fe19
Reham Yahia <asmaa115yahia@gmail.com>
2023-08-29 09:13:03 +0200

    add libraries

---
+++ app/src/main/java/com/example/routinerhabittracker/ui/activities/NavigationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.routinerhabittracker.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.routinerhabittracker.R
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}
