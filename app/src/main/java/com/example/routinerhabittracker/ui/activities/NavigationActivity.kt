27f0eeba054499a79469a263d95c1ded5a5ebb3f
Reham Yahia <asmaa115yahia@gmail.com>
2023-08-30 16:32:56 +0200

    add splash screen

--- app/src/main/java/com/example/routinerhabittracker/ui/activities/NavigationActivity.kt
+++ app/src/main/java/com/example/routinerhabittracker/ui/activities/NavigationActivity.kt
@@ -2,11 +2,18 @@ package com.example.routinerhabittracker.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.routinerhabittracker.R
+import com.example.routinerhabittracker.databinding.ActivityNavigationBinding
 
 class NavigationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNavigationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_navigation)
+        binding = ActivityNavigationBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navControler = navHost.navController
     }
 }
--- app/src/main/java/com/example/routinerhabittracker/ui/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/routinerhabittracker/ui/fragments/SplashFragment.kt
@@ -1,18 +1,23 @@
 package com.example.routinerhabittracker.ui.fragments
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.routinerhabittracker.R
+import com.example.routinerhabittracker.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
-
-
+    private  var _binding: FragmentSplashBinding?=null
+    private val binding get()= _binding!!
+    private lateinit var navControler: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        navControler = findNavController()
     }
 
     override fun onCreateView(
@@ -20,11 +25,17 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+                              val action = SplashFragmentDirections.actionSplashFragmentToSplashPager1Fragment()
+            navControler.navigate(action)
+        } , 9000)
     }
 
 }
