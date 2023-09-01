03a45178c250d62dbf4dd007e20e64a72a8f8063
Reham Yahia <asmaa115yahia@gmail.com>
2023-09-01 17:13:32 +0200

    add view pager

---
+++ app/src/main/java/com/example/routinerhabittracker/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.routinerhabittracker.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+
+class ViewPagerAdapter(list:ArrayList<Fragment> , fragment:FragmentManager , lifeCycle: Lifecycle) : FragmentStateAdapter (fragment , lifeCycle){
+    val fragmentList :ArrayList<Fragment> = list
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+}
--- app/src/main/java/com/example/routinerhabittracker/ui/fragments/splash_screens/ViewPagerSplashFragment.kt
+++ app/src/main/java/com/example/routinerhabittracker/ui/fragments/splash_screens/ViewPagerSplashFragment.kt
@@ -1,18 +1,20 @@
 package com.example.routinerhabittracker.ui.fragments.splash_screens
 
 import android.os.Bundle
-import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.example.routinerhabittracker.R
+import com.example.routinerhabittracker.adapter.ViewPagerAdapter
 import com.example.routinerhabittracker.databinding.FragmentSplashBinding
+import com.example.routinerhabittracker.databinding.FragmentViewPagerSplashBinding
 
 
-class SplashFragment : Fragment() {
-    private  var _binding: FragmentSplashBinding?=null
+class ViewPagerSplashFragment : Fragment() {
+    private  var _binding: FragmentViewPagerSplashBinding?=null
     private val binding get()= _binding!!
     private lateinit var navControler: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,18 +27,23 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        _binding = FragmentViewPagerSplashBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        val fragmentList = arrayListOf<Fragment>(
+                SplashPager1Fragment(),
+                SplashPager2Fragment(),
+                SplashPager3Fragment()
+        )
+
+        val adapter = ViewPagerAdapter(
+            fragmentList,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+        binding.splashViewPager.adapter = adapter
         return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        Handler().postDelayed({
-                              val action =com.example.routinerhabittracker.ui.fragments.splash_screens.SplashFragmentDirections.actionSplashFragmentToViewPagerSplashFragment()
-
-            navControler.navigate(action)
-        } , 9000)
-    }
 
 }
